115976bd9346dacb4e451ea98201b76ec2183967
Piotr Krzysztof <piotr@codextechnologies.org>
2026-02-22 19:51:46 +0100

    feat: euclid algorithm written in c

---
+++ euclid/euclid.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int solve(int m, int n) {
+
+  // E0
+  if (n > m) {
+    int t = m;
+    m = n;
+    n = t;
+  }
+
+  // E1
+  int d = m / n;
+  int r = m % n;
+
+  // E2
+  if (r == 0) {
+    return n;
+  }
+
+  // E3
+  return solve(n, r);
+}
+
+int main() { printf("%d\n", solve(544, 119)); }
