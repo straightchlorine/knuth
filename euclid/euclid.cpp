118dd0a35cd7910e9186c4b01815965fbad9ed5a
Piotr Krzysztof <piotr@codextechnologies.org>
2026-02-22 19:41:14 +0100

    feat: euclids algorithm written in c++

---
+++ euclid/euclid.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+class Euclid {
+private:
+  int r = -1;
+
+public:
+  int solve(int m, int n);
+};
+
+int Euclid::solve(int m, int n) {
+
+  // E0
+  if (m < n) {
+    int t = m;
+    m = n;
+    n = t;
+  }
+
+  // E1
+  int d = m / n;
+  r = m % n;
+
+  // E2
+  if (r == 0) {
+    return n;
+  };
+
+  // E2
+  return solve(n, r);
+}
+
+int main() {
+  Euclid euclid;
+  int result = euclid.solve(544, 119);
+  std::cout << result << std::endl;
+  return result;
+}
